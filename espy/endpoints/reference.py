9d8363a938c7e7d2b31efbcbed66f1df9ab1491d
Ben <benjamin.g.sage@gmail.com>
2025-01-31 03:11:17 -0700

    updates

--- espy/endpoints/reference.py
+++ espy/endpoints/reference.py
@@ -1,5 +1,8 @@
 from base import BaseEndpoints
 
+def split_id(url):
+    return url.split('/')[-1].split('?')[0]
+
 class ReferenceSportsEndpoints(BaseEndpoints):
     base_url = "https://sports.core.api.espn.com/v2/sports/{}/leagues/{}/"
     supported_sports = ["football", "basketball", "baseball"]
@@ -9,6 +12,9 @@ class ReferenceSportsEndpoints(BaseEndpoints):
         super().__init__(sport, league)
         self.base_url = self.base_url.format(sport, league)
     
+    def __split_id(self, url):
+        return url.split('/')[-1].split('?')[0]
+
     #TODO: active flag doesn't seem to work.
     def _get_athletes(self, limit: int=1000, active: bool=True, page: int=1):
         query = self._build_query(False, limit=limit, active=active, page=page)
@@ -20,7 +26,7 @@ class ReferenceSportsEndpoints(BaseEndpoints):
         pg = 1
         while True:
             athletes = self._get_athletes(page=pg)
-            ids += [item['$ref'].split('/')[-1].split('?')[0] for item in athletes['items']]
+            ids += [split_id(item['$ref']) for item in athletes['items']]
             if athletes['pageIndex'] == athletes['pageCount']:
                 return ids
             pg += 1
@@ -29,12 +35,13 @@ class ReferenceSportsEndpoints(BaseEndpoints):
         query = self._build_query(False, limit=limit, page=page)
         return self._execute_request("teams" + query)
 
+    #TODO: this looks a lot like get_athlete_ids... can generalize some of this logic
     def get_team_ids(self):
         ids = []
         pg = 1
         while True:
             teams = self._get_teams(page=pg)
-            ids += [item['$ref'].split('/')[-1].split('?')[0] for item in teams['items']]
+            ids += [split_id(item['$ref']) for item in teams['items']]
             if teams['pageIndex'] == teams['pageCount']:
                 return ids
             pg += 1
